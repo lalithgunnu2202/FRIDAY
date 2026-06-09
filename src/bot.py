fe2feb61d58350f039f0149738c22dffc932cf27
Lalith <lalithgunnu2202@gmail.com>
2026-06-09 18:08:48 +0530

    sales agent done

---
+++ src/bot.py
@@ -0,0 +1,122 @@
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import sys
+import os
+
+# ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+# sys.path.append(ROOT_DIR)
+# # This ensures the directory containing 'bot.py' is in the search path
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# Now you can import 'ans' directly without dots or 'online'
+from sales_agent import sales_agent
+# from src.components.main import send_text
+# from src.logger import logging
+
+load_dotenv()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Send a welcome message when /start is issued."""
+    user = update.effective_user
+    welcome_text = f"""
+👋 Hello {user.first_name}!
+
+I'm an AI assistant powered by SPES AI.
+
+Available commands:
+/start - To Start me
+/help - Get help
+/ask <question> - Ask me anything!
+
+Just send me a message and I'll respond!
+    """
+    await update.message.reply_text(welcome_text)
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Send help message."""
+    help_text = """
+🤖 **How to use this bot:**
+
+1. Just type your question and send it!
+2. Use commands:
+   /ask <question> - Ask a question
+   /help - This message
+   Else Type any message i can handle it
+    """
+    await update.message.reply_text(help_text, parse_mode='Markdown')
+
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE, text=None):
+    user_text = text or update.message.text
+    user_ph_no = update.effective_user.id
+    # logging.info("Recieved user text")
+    # Show "typing" indicator
+    await update.message.chat.send_action(action="typing")
+    try:
+        response = sales_agent(user_text,user_ph_no)
+        
+        # Send response (Telegram has 4096 character limit)
+        # if len(response) > 4000:
+        #         response = response[:4000] + "...\n\n[Response truncated due to length]"
+
+        if len(response)==2:    
+            await update.message.reply_photo(
+                photo=response[1],
+                caption=response[0]
+            )
+        elif len(response)==1:
+            await update.message.reply_text(response[0])
+
+    except Exception as e:
+        error_msg = f"❌ Error: {str(e)[:100]}..."
+        # logging.error("Error while process the user request!!")
+        # More specific error messages
+        if "rate limit" in str(e).lower() or "429" in str(e):
+            error_msg = "⚠️ Rate limit exceeded. Free models allow ~2 requests per minute. Please wait a moment."
+        elif "404" in str(e):
+            error_msg = "❌ Model not found. The AI model might be unavailable. Try changing the model in code."
+        
+        await update.message.reply_text(error_msg)
+
+async def error_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Log errors."""
+    # logging.error(f"Update {update} caused error {context.error}")
+
+def main():
+    """Start the bot."""
+    # Get bot token from environment
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    
+    if not token:
+        # logging.error("TELEGRAM_BOT_TOKEN not found in environment variables!")
+        print("❌ Please set TELEGRAM_BOT_TOKEN in your .env file")
+        print("Get a token from @BotFather on Telegram")
+        return
+    
+    # Create Application
+    application = Application.builder().token(token).build()
+    
+    # Add command handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    # application.add_handler(CommandHandler("model", show_model))
+    # application.add_handler(CommandHandler("ask", ask_command))
+    
+    # Add message handler for regular messages
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    
+    # Add error handler
+    application.add_error_handler(error_handler)
+    
+    # Start the bot
+    print("🤖 Bot is starting...")
+    print(f"✅ Using token: {token[:10]}...")
+    print("Press Ctrl+C to stop")
+    
+    # Run the bot until Ctrl+C
+    application.run_polling(stop_signals=False,allowed_updates=Update.ALL_TYPES)
+
+if __name__ == '__main__':
+    main()
---
+++ src/sales_agent.py
@@ -0,0 +1,191 @@
+from langchain.chat_models import init_chat_model
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient, TEXT
+from pymongo.collection import Collection
+from typing_extensions import Annotated, TypedDict
+from langgraph.graph.message import add_messages
+from datetime import datetime,timezone
+from pydantic import BaseModel, Field
+from langchain_openrouter import ChatOpenRouter
+from enum import Enum
+
+load_dotenv()
+os.environ["OPENROUTER_API_KEY"]=os.getenv("CUSTOM_API_KEY")
+
+llm = ChatOpenRouter(model="nvidia/nemotron-nano-9b-v2:free")
+print(llm)
+
+def get_collection(db_name:str,col_name:str)->Collection:
+    client = MongoClient(os.getenv("MONGO_URI"), serverSelectionTimeoutMS=5000)
+    db=client[db_name]
+    collection=db[col_name] #collections are different for different user. i will manage them in my mongodb
+    return collection
+    
+class MemoryManager:
+    def __init__(self, collection):
+        self.collection = collection
+
+    def update(self, user_id, **kwargs):
+        kwargs["updated_at"] = datetime.now(timezone.utc)
+
+        self.collection.update_one(
+            {"user_id": user_id},
+            {"$set": kwargs},
+            upsert=True
+        )
+
+    def get(self, user_id):
+        return self.collection.find_one({"user_id": user_id})  #should use **state["memory"] while i am willing to update only particular field
+
+short_term_memory=MemoryManager(get_collection("Spes-AI","short-term-memory"))
+products=get_collection("Spes-AI","products")
+
+class State(TypedDict):
+    messages:Annotated[list,add_messages]
+    price:  float | None=None
+    user_id: str|None=None
+    # memory= dict | None=None
+
+class Intent(str, Enum):
+    BROWSE_PRODUCTS="get_products"
+    FOLLOW_UP_QUESTIONS="follow_up"
+    BUY_PRODUCT="buy_product"
+
+class productQuery(BaseModel):
+    product_type: str | None=None
+    color: str | None=None
+    max_price: float| None=None
+    product_id: str | None=None
+    intent: Intent | None = Field(
+        default=None,
+        description="""
+        User intent.
+
+        get_products: User wants product recommendations or product search or see the products.
+        follow_up: User is asking a question regarding previous product. either by mentioning it or without mentioning it
+        """
+    )
+
+structured_llm=llm.with_structured_output(productQuery)
+
+def get_products(result: str, state:State):
+    print(result)
+    if result.product_id:
+        prod=products.find_one(
+            {"prod_id": result.product_id},
+            {"_id": 0}
+        )
+        if prod:
+            return [prod]
+        else:
+            return []
+
+    filters = []
+
+    if result.product_type:
+        filters.append({
+            "$or": [
+                {"title": {"$regex": result.product_type, "$options": "i"}},
+                {"body_html": {"$regex": result.product_type, "$options": "i"}},
+                {"tags": {"$regex": result.product_type, "$options": "i"}},
+                {"type": {"$regex": result.product_type, "$options": "i"}},
+                {"handle": {"$regex": result.product_type, "$options": "i"}},
+            ]
+        })
+
+    if result.max_price:
+        filters.append({
+            "price": {"$lte": result.max_price}
+        })
+
+    mongo_filter = {"$and": filters} if filters else {}
+    prods=list(products.find(mongo_filter, {"_id": 0}).sort("orders", -1).limit(3))
+    if len(prods):
+        return prods
+    else:
+        return []
+
+# print(get_products("show products under 130 rupees"))
+
+def final_prods(result:str,state:State):
+    
+    # userid="abc12"
+    # state={
+    #     "messages":[],
+    #     "user_id":"abc12"
+    # }
+    prods=get_products(result,state)
+    if len(prods)>1:
+        reply=[]
+        for prod in prods:
+            title=prod["title"]
+            desc=prod["body_html"]
+            price=prod["price"]
+            prod_id=prod["prod_id"]
+            msg=f"name: {title} \ndescription: {desc} \nprice: {price} product id: {prod_id} \nUse product id to choose product"
+            reply.append(msg)
+        reply2="\n\n".join(reply)
+        return [reply2]
+    elif len(prods)==1:
+        prod=prods[0]
+        title=prod["title"]
+        desc=prod["body_html"]
+        price=prod["price"]
+        img_link=prod["image_src"]
+        prod_id=prod["prod_id"]
+        short_term_memory.update(state["user_id"],**{"prod_id":prod_id})
+        msg=f"name: {title} \ndescription: {desc} \nprice: {price} product id: {prod_id}"
+        return [msg,img_link]
+    else:
+        return ["No product found"]
+
+
+def follow_up(query,state:State):
+    """this is a follow-up question answering tool. if query looks like a followup question about any product. choose this tool"""
+    memory=short_term_memory.get(state["user_id"])
+    prod_id=memory["prod_id"]
+    short_term_memory.update(state["user_id"],**{"prod_id":prod_id})
+    prod=products.find_one(
+            {"prod_id": prod_id},
+            {"_id": 0}
+        )
+    if prod:
+        prompt = f"""
+            Current Product:
+
+            {prod}
+
+            User Question:
+
+            {query}
+
+            Answer only using the product information. and write a ctr message to convert them to buyers. try to answer the users with as short as possible.
+            """
+        response=llm.invoke(prompt)
+        return [response.content]
+    else:
+        return []
+
+def buy_agent(query):
+    return ["thanks for buyign this product"]
+
+# print(final_prods("show product a5"))
+def sales_agent(query, user_id):
+    state={
+        "message":[],
+        "price":None,
+        "user_id":user_id,
+    }
+    result = structured_llm.invoke(query)
+    print(result)
+    if result.intent==Intent.BROWSE_PRODUCTS:
+        return final_prods(result,state)
+        
+    if result.intent==Intent.BUY_PRODUCT:
+        return buy_agent(query)
+    if result.intent==Intent.FOLLOW_UP_QUESTIONS:
+        return follow_up(query,state)
+    return ["I am having trouble understanding that. Could you rephrase?"]
+    
+# print(llm.invoke("what is ai"))
